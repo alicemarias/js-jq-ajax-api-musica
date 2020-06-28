af4f662f9d5f819a962754b242415e403417dd8e
alice maria spina <alicem.spina93@gmail.com>
2020-06-28 22:24:12 +0200

    commit es finito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	//Code
+	$.ajax(
+    {
+      url: 'https://flynn.boolean.careers/exercises/api/array/music',
+      method: 'GET',
+      success: function(dataResponse) {
+        var dischi= dataResponse.response;
+        stampaCd(dischi);
+      },
+      error: function() {
+        alert:("errore");
+      }
+    }
+  );
+  function stampaCd(arrayDischi) {
+
+    var source = $('#cd-template').html();
+    var template = Handlebars.compile(source);
+
+    for (var i = 0; i < arrayDischi.length; i++) {
+      var singoloCd = arrayDischi[i];
+      var html = template(singoloCd);
+      $('.cds-container').append(html);
+    }
+  }
 });
